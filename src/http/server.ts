944d3f4227bed7b4c455443d41a55a6ad0d1bf4b
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-09-11 17:28:05 -0300

    feat: :sparkles: criando rota de post de goals

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,37 @@
 import fastify from 'fastify'
+import { createGoal } from '../functions/create-goal'
+import z from 'zod'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z
+        .object({
+          title: z.string(),
+          desiredWeeklyFrequency: z.number().int().min(1).max(7),
+        })
+        .strip(),
+    },
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP Server running on http://localhost:3333')
