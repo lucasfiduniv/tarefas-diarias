29f1edaf9a4324a5be00014db8ddb06bada07ceb
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-09-11 17:26:35 -0300

    fix: :bug: adicionando mais dados na seed

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,23 +1,44 @@
-import { db } from '.'
+import dayjs from 'dayjs'
+import { client, db } from '.'
 import { goalCompletations, goals } from './schema'
 
 async function seed() {
   await db.delete(goalCompletations)
   await db.delete(goals)
 
-  await db.insert(goals).values([
+  const result = await db
+    .insert(goals)
+    .values([
+      {
+        title: 'Teste 1',
+        desiredWeeklyFrequency: 2,
+      },
+      {
+        title: 'Teste 2',
+        desiredWeeklyFrequency: 3,
+      },
+      {
+        title: 'Teste 3',
+        desiredWeeklyFrequency: 4,
+      },
+    ])
+    .returning()
+
+  const startOfWeek = dayjs().startOf('week')
+  await db.insert(goalCompletations).values([
     {
-      title: 'Teste 1',
-      desiredWeeklyFrequency: 2,
+      goalId: result[0].id,
+      completedAt: startOfWeek.toDate(),
     },
     {
-      title: 'Teste 2',
-      desiredWeeklyFrequency: 3,
+      goalId: result[1].id,
+      completedAt: startOfWeek.add(1, 'day').toDate(),
     },
     {
-      title: 'Teste 3',
-      desiredWeeklyFrequency: 4,
+      goalId: result[2].id,
+      completedAt: startOfWeek.add(2, 'day').toDate(),
     },
   ])
 }
+seed().finally(() => client.end())
 seed()
