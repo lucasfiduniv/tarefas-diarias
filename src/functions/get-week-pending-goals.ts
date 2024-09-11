eb577bd274c25f28b1eefcb7cc08e60051dc8b1e
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-09-11 18:00:11 -0300

    feat: :sparkles: criando funcao de buscar goals por semana

---
+++ src/functions/get-week-pending-goals.ts
@@ -0,0 +1,38 @@
+import dayjs from 'dayjs'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
+import { db } from '../db'
+import { and, count, gte, lte, sql } from 'drizzle-orm'
+import { goalCompletations, goals } from '../db/schema'
+
+export function getWeekPendingGoals() {
+  const firstDayOfTheWeek = dayjs().startOf('week').toDate()
+  const lastDayOfTheWeek = dayjs().endOf('week').toDate()
+
+  const goalsCreatedUpToWeek = db.$with('goals_created_up_to_week').as(
+    db
+      .select({
+        id: goals.id,
+        title: goals.title,
+        desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+        createdAt: goals.createdAt,
+      })
+      .from(goals)
+      .where(lte(goals.createdAt, lastDayOfTheWeek))
+  )
+
+  const goalCompletationCounts = db.$with('goal_completation_counts').as(
+    db
+      .select({
+        completationCount: count(goalCompletations.id),
+      })
+      .from(goalCompletations)
+      .where(
+        and(
+          gte(goalCompletations.completedAt, firstDayOfTheWeek),
+          lte(goals.createdAt, lastDayOfTheWeek)
+        )
+      )
+
+      .groupBy(goalCompletations.goalId)
+  )
+}
