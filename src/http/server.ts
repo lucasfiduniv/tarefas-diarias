0ba523675d01f6dc8178491e7d504f486b98d073
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-09-11 08:25:04 -0300

    feat: :sparkles: criando porta para o fastify

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,7 @@
-console.log("hello World");
+import fastify from "fastify";
+
+const app = fastify();
+
+app.listen({ port: 3333 }).then(() => {
+    console.log("HTTP Server running on http://localhost:3333");
+});
