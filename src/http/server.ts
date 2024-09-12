8f5bc69746aab0997a94c59c09f0634e9f9d09c5
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-09-12 14:31:41 -0300

    fix: :bug: adicionando route no nome das rotas

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,24 +1,21 @@
 import fastify from 'fastify'
-
-import z from 'zod'
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
-import { createGoalCompletion } from '../functions/create-goal-completion'
+
 import { createGoalRoute } from './routes/create-goal'
-import { getPendingGoals } from './routes/get-pending-goals'
-import { createCompletion } from './routes/create-completion'
+import { getPendingGoalsRoute } from './routes/get-pending-goals'
+import { createCompletionRoute } from './routes/create-completion'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-app.register(createCompletion)
-app.register(getPendingGoals)
+app.register(createCompletionRoute)
+app.register(getPendingGoalsRoute)
 app.register(createGoalRoute)
 
 app.listen({ port: 3333 }).then(() => {
