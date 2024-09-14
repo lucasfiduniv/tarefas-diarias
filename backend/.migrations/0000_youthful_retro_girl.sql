e2d0f8c2498bb9519f3b357f780f6f360a0f30b0
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-09-13 23:11:44 -0300

    fix: :bug: adicionadno a pagina inicial do app

--- backend/.migrations/0000_youthful_retro_girl.sql
+++ backend/.migrations/0000_youthful_retro_girl.sql
@@ -4,6 +4,13 @@ CREATE TABLE IF NOT EXISTS "goal_completions" (
 	"completed_at" timestamp with time zone DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "goals" (
+	"goal_id" text PRIMARY KEY NOT NULL,
+	"title" text NOT NULL,
+	"desired_weekly_frequency" integer NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "goal_completions" ADD CONSTRAINT "goal_completions_goal_id_goals_goal_id_fk" FOREIGN KEY ("goal_id") REFERENCES "public"."goals"("goal_id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import { Plus } from "lucide-react";
+
 function App() {
   return (
     <>
-      <div>
-        <img src="/logo-empityScreen.svg" alt="" />
+      <div className="h-screen flex flex-col justify-center items-center gap-8">
+        <img src="/logo-empityScreen.svg" alt="Logo" />
+        <img src="/lets-start.svg" alt="Logo" />
+        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?</p>
+        <button type="button" className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2"><Plus className="size-4"/>Cadastrar Meta</button>
       </div>
     </>
   );
