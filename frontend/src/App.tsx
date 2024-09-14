7bbd3695a66b3b69c63a554b1e777e2b0531b4ce
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-09-14 14:29:05 -0300

    fix: :bug: adicionando tipagem a interatividade do backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,49 @@
-
-import {
-  Dialog,
-} from "./components/ui/dialog";
+import { Dialog } from "./components/ui/dialog";
 import { CreateGoal } from "./components/create-goal";
-import { EmptyGoals } from "./components/empity-goals";
 import { Summary } from "./components/summary";
+import { useEffect, useState } from "react";
+import api from "./services/api";
+
+interface Goal {
+  goalId: string;
+  title: string;
+  completedAt: string;
+}
+
+interface SummaryData {
+  completed: number;
+  total: number;
+  goalsPerDay: {
+    [date: string]: Goal[];
+  };
+}
+
+interface ApiResponse {
+  summary: SummaryData[];
+}
 
 function App() {
+  const [summary, setSummary] = useState<SummaryData | null>(null); 
+
+  useEffect(() => {
+    const request = async () => {
+      try {
+        const response = await api.get<ApiResponse>("summary");
+        setSummary(response.data.summary[0]); 
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    request();
+  }, []);
+
   return (
     <Dialog>
-     {/* <EmptyGoals/> */}
-     <Summary/>
-     <CreateGoal/>
+      {summary === null ? (
+        <CreateGoal />
+      ) : (
+        <Summary summaryData={summary} /> 
+      )}
     </Dialog>
   );
 }
