0110c56212d77a2648181fb7d76954b905386c4b
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-09-14 10:43:32 -0300

    feat: :sparkles: criando o componente summary

---
+++ frontend/src/components/summary.tsx
@@ -0,0 +1,37 @@
+import { Plus } from "lucide-react";
+import { Button } from "./ui/button";
+import { DialogTrigger } from "./ui/dialog";
+import { InOrbitIcon } from "./in-orbit-icon";
+import { Progress, ProgressIndicator } from "./ui/progress-bar";
+import { Separator } from "./ui/separator";
+
+export function Summary() {
+  return (
+    <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+        <InOrbitIcon/>
+        <span className="text-lg font-semibold">5 a 10 de agosto</span>
+        </div>
+        <DialogTrigger asChild>
+          <Button size="sm">
+            <Plus className="size-4" />
+            Cadastrar Meta
+          </Button>
+        </DialogTrigger>
+      </div>
+      <div className="flex flex-col gap-3">
+        <Progress value={5} max={15} >
+          <ProgressIndicator style={{width: '50%'}}/>
+        </Progress>
+<div className="flex items-center justify-between text-xm text-zinc-400">
+  <span>Você completou <span className="text-zinc-100">8</span> de <span className="text-zinc-100">15</span> metas nessa semana.</span>
+  <span>58%</span>
+</div>
+<div>
+  <Separator/>
+</div>
+      </div>
+    </div>
+  );
+}
