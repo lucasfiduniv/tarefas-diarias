a7ab209b40205b3c3b88e9ecdda8b97f6c59b698
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-09-14 10:01:39 -0300

    fix: :bug: criando botoes de dentro do formulario

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
 import { Button } from "./components/ui/button";
-import { Dialog, DialogContent, DialogTrigger } from "./components/ui/dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "./components/ui/dialog";
 
 function App() {
   return (
@@ -8,19 +15,43 @@ function App() {
       <div className="h-screen flex flex-col justify-center items-center gap-8">
         <img src="/logo-empityScreen.svg" alt="Logo" />
         <img src="/lets-start.svg" alt="Logo" />
-        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?</p>
+        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+          Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora
+          mesmo?
+        </p>
         <DialogTrigger asChild>
-          <Button >
+          <Button>
             <Plus className="size-4" />
             Cadastrar Meta
           </Button>
         </DialogTrigger>
       </div>
       <DialogContent>
-        oi
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-3">
+            <div className="flex items-center justify-between">
+              <DialogTitle>Cadastrar Meta</DialogTitle>
+              <DialogClose>
+                <X className="size-5 text-zinc-600" />
+              </DialogClose>
+            </div>
+            <DialogDescription>
+              Adicione atividades que te fazem bem e que você quer continuar
+              praticando toda semana.
+            </DialogDescription>
+          </div>
+          <form action="" className="flex-1 flex flex-col justify-between">
+            <div>
+
+            </div>
+            <div className="flex items-center gap-3">
+              <Button variant="secondary" className="flex-1">Fechar</Button>
+                <Button className="flex-1">Salvar</Button>
+            </div>
+          </form>
+        </div>
       </DialogContent>
     </Dialog>
-
   );
 }
 
--- frontend/src/components/ui/dialog.tsx
+++ frontend/src/components/ui/dialog.tsx
@@ -40,7 +40,7 @@ export function DialogContent(props: DialogPrimitive.DialogContentProps) {
 
 export function DialogTitle(props: DialogPrimitive.DialogTitleProps) {
   return (
-    <DialogPrimitive.DialogTitle {...props} className="text-lg font-semibold" />
+    <DialogPrimitive.DialogTitle {...props} className="text-lg font-semibold text-zinc-200" />
   )
 }
 
