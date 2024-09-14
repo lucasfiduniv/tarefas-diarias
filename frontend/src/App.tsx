fdc5be8b3c83553e5da483fc163a9f93a5d8052b
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-09-14 09:51:52 -0300

    fix: :bug: criando o resto dos componentes e utilizando o dialog na pagina principal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,26 @@
 import { Plus } from "lucide-react";
+import { Button } from "./components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "./components/ui/dialog";
 
 function App() {
   return (
-    <>
+    <Dialog>
       <div className="h-screen flex flex-col justify-center items-center gap-8">
         <img src="/logo-empityScreen.svg" alt="Logo" />
         <img src="/lets-start.svg" alt="Logo" />
         <p className="text-zinc-300 leading-relaxed max-w-80 text-center">Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?</p>
-        <button type="button" className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2"><Plus className="size-4"/>Cadastrar Meta</button>
+        <DialogTrigger asChild>
+          <Button >
+            <Plus className="size-4" />
+            Cadastrar Meta
+          </Button>
+        </DialogTrigger>
       </div>
-    </>
+      <DialogContent>
+        oi
+      </DialogContent>
+    </Dialog>
+
   );
 }
 
