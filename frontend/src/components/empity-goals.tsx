8d8b2093e62d5af520e029660c105e12b62cd625
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-09-14 10:43:02 -0300

    feat: :sparkles: criando o componente empity-goals

---
+++ frontend/src/components/empity-goals.tsx
@@ -0,0 +1,22 @@
+import { Plus } from "lucide-react";
+import { Button } from "./ui/button";
+import { DialogTrigger } from "./ui/dialog";
+
+export function EmptyGoals() {
+  return(
+    <div className="h-screen flex flex-col justify-center items-center gap-8">
+    <img src="/logo-empityScreen.svg" alt="Logo" />
+    <img src="/lets-start.svg" alt="Logo" />
+    <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+      Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora
+      mesmo?
+    </p>
+    <DialogTrigger asChild>
+      <Button>
+        <Plus className="size-4" />
+        Cadastrar Meta
+      </Button>
+    </DialogTrigger>
+  </div>
+  )
+}
