8842368fc649e08c15ef208f6331a940ea1e827a
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-09-17 10:24:58 -0300

    fix; :bug: criando funcao de goalCompletation

--- frontend/src/components/pending-goals.tsx
+++ frontend/src/components/pending-goals.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getPendingGoals } from "../http/get-pending-goals";
 import { OutlineButton } from "./ui/outline-button";
 import { Plus } from "lucide-react";
+import { createGoalCompletion } from "../http/create-goal.completion";
 
 export function PendingGoals() {
   const { data } = useQuery({
@@ -11,11 +12,18 @@ export function PendingGoals() {
   });
   if (!data) return null;
 
+  async function handleGoalCompletion(goalId: string) {
+    await createGoalCompletion(goalId);
+  }
   return (
     <div className="flex flex-wrap gap-3">
       {data.map((goal) => {
         return (
-          <OutlineButton>
+          <OutlineButton
+            key={goal.id}
+            disabled={goal.completationCount >= goal.desiredWeeklyFrequency}
+            onClick={() => handleGoalCompletion(goal.id)}
+          >
             <Plus className="size-4" />
             {goal.title}
           </OutlineButton>
