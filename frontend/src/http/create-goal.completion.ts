5d5bcbf488a73611e355b61496d73ec71c2c5e66
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-09-17 10:25:18 -0300

    feat: :sparkles: criando rota de goalCompletion

---
+++ frontend/src/http/create-goal.completion.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+import api from "../services/api";
+
+export function createGoalCompletion(goalId: string) {
+  return api.post(`/goal-completions`, {
+    goalId: goalId,
+  });
+}
